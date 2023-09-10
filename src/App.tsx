5bcf31389d6f796f4fcc147aa780f67af3b52184
ykhomitsky <yura.khomitsky8@gmail.com>
2023-09-10 11:03:34 +0300

    Create MovieDetails component and refactor a bit code

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,51 @@
 import { ReactElement, useState } from 'react';
+import PosterImg from './assets/Poster.png';
 
 import './App.css';
 import { Header } from './Header/Header.tsx';
 import { Card, GenreSelect, SearchForm, SortBy } from './components';
-import { MoviesList } from './Movies';
+import { MovieDetails, MovieModel, MoviesList } from './Movies';
 
 function App(): ReactElement {
 	const [searchTerm, setSearchTerm] = useState('');
+	const [selectedMovie, setSelectedMovie] = useState<MovieModel | null>(null);
 	const [selectedGenre, setGenre] = useState<string | null>('All');
 	const [selectedSortOption, setSortOption] = useState<string | null>('Release Date');
-	const genres= ['All', 'Documentary', 'Comedy', 'Horror', 'Crime']
+	const genres = ['All', 'Documentary', 'Comedy', 'Horror', 'Crime'];
 	const sortByOptions = ['Release Date', 'Title'];
 
-	return (
-		<>
-			<Header>
-				<SearchForm searchTerm={searchTerm} onSearch={setSearchTerm}></SearchForm>
-			</Header>
-			<main>
+	const movieDetails: MovieModel = {
+		image: PosterImg, name: 'SpiderMan', releaseYear: '2004', genres: ['Fight', 'Fantastic'],
+		rating: 8.9,
+		duration: '2h 34min',
+		description: `Jules Winnfield (Samuel L. Jackson) and Vincent Vega (John Travolta) are two hit men who are out to retrieve a suitcase stolen from their employer, mob boss Marsellus Wallace (Ving Rhames). 
+		Wallace has also asked Vincent to take his wife Mia (Uma Thurman) out a few days later when Wallace himself will be out of town. 
+		Butch Coolidge (Bruce Willis) is an aging boxer who is paid by Wallace to lose his fight. 
+		The lives of these seemingly unrelated people are woven together comprising of a series of funny, 
+		bizarre and uncalled-for incidents.—Soumitra`
+	};
+	return (<>
+		{
+			selectedMovie ?
 				<Card>
-					<header className="header-container">
-						<GenreSelect selectedGenre={selectedGenre} genres={genres} onSelect={setGenre}></GenreSelect>
-						<SortBy options={sortByOptions} selectedOption={selectedSortOption} onSelect={setSortOption}></SortBy>
-					</header>
-					<MoviesList movies={[{ image: '', name: 'SpiderMan', releaseYear: '2004', genres: ['Fight', 'Fantastic'] }]}></MoviesList>
-				</Card>
-			</main>
+					<MovieDetails movie={movieDetails}></MovieDetails>
+				</Card> :
+				<Header>
+					<SearchForm searchTerm={searchTerm} onSearch={setSearchTerm}></SearchForm>
+				</Header>
+		}
 
-		</>
-	);
+		<main>
+			<Card>
+				<header className="header-container">
+					<GenreSelect selectedGenre={selectedGenre} genres={genres} onSelect={setGenre}></GenreSelect>
+					<SortBy options={sortByOptions} selectedOption={selectedSortOption} onSelect={setSortOption}></SortBy>
+				</header>
+				<MoviesList movies={[movieDetails]} onMovieClick={setSelectedMovie}></MoviesList>
+			</Card>
+		</main>
+
+	</>);
 }
 
 export default App;
---
+++ src/Movies/MovieDetails.tsx
@@ -0,0 +1,27 @@
+import { MovieDetailsModel } from './movie-details.model.ts';
+import { ReactElement } from 'react';
+import styles from './MovieDetails.module.css';
+import { MovieGenres } from './MovieGenres.tsx';
+
+interface MovieDetailsProps {
+	movie: MovieDetailsModel;
+}
+
+export function MovieDetails({ movie }: MovieDetailsProps): ReactElement {
+	return <section className={styles.movieDetailsContainer}>
+		<img src={movie.image} alt={movie.name}></img>
+		<div className={styles.movieDetails}>
+			<div className={styles.movieTitle}>
+				<h2>{movie.name}</h2>
+				<span>{movie.rating}</span>
+			</div>
+
+			<MovieGenres genres={movie.genres}></MovieGenres>
+			<div className={styles.movieReleaseAndDuration}>
+				<span>{movie.releaseYear}</span>
+				<span>{movie.duration}</span>
+			</div>
+			<p className={styles.movieDescription}>{movie.description}</p>
+		</div>
+	</section>;
+}
